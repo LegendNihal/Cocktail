c4caa0fd855d17023b83174c14e5481159a7a3a1
unknown <adinnihalnz@gmail.com>
2022-06-26 19:46:40 +0600

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nemo cumque reiciendis eaque alias deserunt, incidunt repellat eligendi odio accusamus, ipsa quam natus voluptate tempora, mollitia ipsum ratione non. Error neque fugit laborum voluptates aspernatur, optio labore aut ratione, perferendis velit dolore. Eos odit voluptatem doloremque quaerat at ut. Corporis repudiandae, quam earum fuga placeat illo, facere accusantium iure accusamus enim doloribus, aperiam dolores natus distinctio dolorem? Nemo neque itaque eligendi consequatur placeat, eveniet amet, natus possimus soluta deserunt eius rem vitae vel saepe fugiat quia quasi, eaque odio laudantium. Sequi praesentium similique at neque. Quisquam accusantium obcaecati assumenda eligendi?</p>
+    </section>
+  )
+}
+
+export default About
